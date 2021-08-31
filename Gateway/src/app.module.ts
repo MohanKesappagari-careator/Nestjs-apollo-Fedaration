c93087809c15ed85b980fd7bc2c99421b82ef059
mohankesappagari <mohan.k@careator.com>
2021-08-31 10:28:33 +0530

    authentication

--- Gateway/src/app.module.ts
+++ Gateway/src/app.module.ts
@@ -1,20 +1,52 @@
+import { RemoteGraphQLDataSource } from '@apollo/gateway';
 import { Module } from '@nestjs/common';
-import { GraphQLGatewayModule } from '@nestjs/graphql';
+import { GATEWAY_BUILD_SERVICE, GraphQLGatewayModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  async willSendRequest({ request, context }) {
+    request.http.headers.set('Authorization', context.jwt);
+  }
+}
+@Module({
+  providers: [
+    {
+      provide: AuthenticatedDataSource,
+      useValue: AuthenticatedDataSource,
+    },
+    {
+      provide: GATEWAY_BUILD_SERVICE,
+      useFactory: (AuthenticatedDataSource) => {
+        return ({ name, url }) => new AuthenticatedDataSource({ url });
+      },
+      inject: [AuthenticatedDataSource],
+    },
+  ],
+  exports: [GATEWAY_BUILD_SERVICE],
+})
+class BuildServiceModule {}
 @Module({
   imports: [
-    GraphQLGatewayModule.forRoot({
-      server: {
-        cors: true,
-      },
-      gateway: {
-        serviceList: [
-          { name: 'owner', url: 'http://localhost:5000/graphql' },
-          { name: 'pet', url: 'http://localhost:5002/graphql' },
-        ],
-      },
+    GraphQLGatewayModule.forRootAsync({
+      useFactory: async () => ({
+        gateway: {
+          serviceList: [
+            { name: 'owner', url: 'http://localhost:5000/graphql' },
+            { name: 'user', url: 'http://localhost:5001/graphql' },
+            { name: 'pet', url: 'http://localhost:5002/graphql' },
+          ],
+        },
+        subscriptions: false,
+        server: {
+          context: ({ req }) => ({
+            jwt: req.headers.authorization,
+          }),
+
+          cors: true,
+        },
+      }),
+      imports: [BuildServiceModule],
+      inject: [GATEWAY_BUILD_SERVICE],
     }),
   ],
   controllers: [AppController],
